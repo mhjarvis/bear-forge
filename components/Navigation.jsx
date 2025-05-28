871f7307f78151bb90673ce520ca517dfdb9334c
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-05-27 21:32:22 -0500

    feat: update Navigation component with new layout and styling

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -2,9 +2,35 @@ import React from "react"
 
 const Navigation = () => {
 	return (
-		<nav className="bg-white shadow-md sticky top-0 z-50">
-			<div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
-				<h1 className="text-xl font-semibold">Bear Forge</h1>
+		<nav className="fixed left-0 top-16 z-50 h-screen w-42 bg-gray-100 p-8 border-r border-gray-300">
+			<h2 className="text-gray-500 uppercase text-xs mb-4">Main Menu</h2>
+			<div className="flex flex-col border-b text-md text-gray-700 border-gray-300">
+				{/* Add your nav items here */}
+				<a href="#" className="mb-4  hover:underline">
+					Dashboard
+				</a>
+				<a href="#" className="mb-4 hover:underline">
+					Builder
+				</a>
+				<a href="#" className="mb-4  hover:underline">
+					Cardio
+				</a>
+				<a href="#" className="mb-4  hover:underline">
+					Calendar
+				</a>
+			</div>
+			<h2 className="text-gray-500 uppercase text-xs mt-4 mb-4">Account</h2>
+			<div className="flex flex-col text-gray-700">
+				{/* Add your nav items here */}
+				<a href="#" className="mb-4  hover:underline">
+					Information
+				</a>
+				<a href="#" className="mb-4  hover:underline">
+					Settings
+				</a>
+				<a href="#" className="mb-4 hover:underline">
+					Log Out{" "}
+				</a>
 			</div>
 		</nav>
 	)
